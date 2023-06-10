bbfa0cef2db954e40f18aa4626746cb03986a598
Danyl Brahan <danylbragan@gmail.com>
2023-06-10 17:31:29 +0200

    the end of my work

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,31 @@
 import PropTypes from "prop-types";
+import css from "./Card.module.css"
 
 export default function Card({name, email, place, folowers, views, likes}) {
     return(
-        <div>
-            <ul>
-                <li><img src="" alt="" /></li>
-                <li><h1>{name}</h1></li>
-                <li><p>{email}</p></li>
-                <li>{place}</li>
-            </ul>
-            <ul>
-                <li>
-                    <p>Folowers</p><br />
-                    <p>{folowers}</p>
-                </li>
-                <li>
-                    <p>Views</p><br />
-                    <p>{views}</p>
-                </li>
-                <li>
-                    <p>Likes</p><br />
-                    <p>{likes}</p>
-                </li>
-            </ul>
+        <div className={css.container}>
+            <div className={css.main_box}>
+                <ul className={css.ul_about}>
+                    <li className={css.li_about}><img src="https://cdn.britannica.com/05/236505-050-17B6E34A/Elon-Musk-2022.jpg" alt="" /></li>
+                    <li className={css.li_about}><h1>{name}</h1></li>
+                    <li className={css.li_about}><p>{email}</p></li>
+                    <li className={css.li_about}><p>{place}</p></li>
+                </ul>
+                <ul className={css.ul_info}>
+                    <li className={css.li_info}>
+                        <p className={css.p_info}>Folowers</p><br />
+                        <p className={css.p_info}>{folowers}</p>
+                    </li>
+                    <li className={css.li_info}>
+                        <p className={css.p_info}>Views</p><br />
+                        <p className={css.p_info}>{views}</p>
+                    </li>
+                    <li className={css.li_info}>
+                        <p className={css.p_info}>Likes</p><br />
+                        <p className={css.p_info}>{likes}</p>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
